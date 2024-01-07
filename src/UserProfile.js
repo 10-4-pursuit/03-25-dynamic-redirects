3229309abfecb16a2f6b40aa64e6aa16f1beaf78
clairep9 <clairepetitfrere@pursuit.org>
2024-01-06 21:36:35 -0500

    Completed Dynamic Redirects exercise

---
+++ src/UserProfile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import userData  from './data/users.json'
+import {useParams, useNavigate} from 'react-router-dom'
+
+const UserProfile =() => {
+    let {userId} = useParams()
+    let navigate = useNavigate()
+    const data = userData.validUserIds
+
+    if (!userId || !data.includes(userId)) {
+        navigate('/profile/default')
+        return `User Profile: ${userId} not found.`;
+    }
+  return (
+    <div>
+        <h1>User Profile</h1>
+        <h2>User ID: {userId}</h2>
+    </div>
+  )
+}
+
+export default UserProfile
