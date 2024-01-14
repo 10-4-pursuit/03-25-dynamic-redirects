eea5857a194f33ffb4ab7f3a3148d5186d66b414
athomas227 <andrewthomas@pursuit.org>
2024-01-14 11:39:04 -0500

    finished exercise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import UserProfile from './UserProfile';
+import users from './data/users.json';
 
 function App() {
+  // Home component rendering a list of user profiles
+  function Home() {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <ul>
+          {users.validUserIds.map(userId => (
+            <li key={userId}>
+              <Link to={`/profile/${userId}`}>Go to {userId}'s Profile</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          {/* Define routes for Home and UserProfile components */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/profile/:userId' element={<UserProfile />}/>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
