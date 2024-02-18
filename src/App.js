0f97b8b5a1dd62cc4f0c6fd145ccf6039513fb50
MoniqueC10-4 <monique.charles13@gmail.com>
2024-02-18 03:01:03 -0500

    Completed Dynamic Redirects exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useParams, useNavigate, BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { useEffect } from 'react';
+import validUserIdsData from '../src/data/users.json';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <Link to='/'>Valid ID List</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<UserList />} /> 
+          <Route path='/profile/:userId' element={<UserProfile />} />
+          <Route path='/profile/default' element={<DefaultPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function UserProfile() {
+  let { userId } = useParams();
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (!validUserIdsData.validUserIds.includes(userId)) {
+      navigate('/profile/default');
+    }
+  }, [userId, navigate]);
+  return (
+      <div>
+        <h1>User Profile</h1>
+        <p>User ID: {userId}</p>
+      </div>
+    )  
+}
+
+function DefaultPage() {
+  return (
+    <div>
+      <h1>Default Page</h1>
+      <p>Invalid user ID!</p>
+    </div>
+  );
+}
+
+function UserList() {
+  return (
+    <div>
+      <h1>User Profiles</h1>
+        {validUserIdsData.validUserIds.map((userId) => (
+          <div key={userId}>
+            <Link to={`/profile/${userId}`}>{userId}</Link>
+          </div>
+        ))}
     </div>
   );
 }
